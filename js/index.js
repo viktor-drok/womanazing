a90e27281be4973cb9cec15298e36ed570aa8af3
viktor-drok <vityadrok@gmail.com>
2022-12-15 23:14:56 +0200

    add breakpoints

--- js/index.js
+++ js/index.js
@@ -7,6 +7,8 @@ burger.addEventListener("click", function () {
 
     if (burger.hasAttribute('is-active')) {
         modalNav.style.transform = 'translateY(0)';
+        // modalNav.style.position = 'relative';
+        modalNav.style.zIndex = '2';
     }
     else {
         modalNav.style.transform = 'translateY(-101%)';
@@ -20,11 +22,28 @@ var swiper1 = new Swiper(".cards", {
         rows: 3,
     },
     pagination: {
-        el: ".swiper-pagination",
+        el: ".swiper-pagination.pagination",
         clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        }
     },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        989: {
+            slidesPerView: 3
+        }
     }
 });
+
+
