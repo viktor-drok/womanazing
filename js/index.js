80298becb1a340e9b22a02cce7d8add9c64c140f
viktor-drok <vityadrok@gmail.com>
2022-12-17 22:32:25 +0200

    add styles

--- js/index.js
+++ js/index.js
@@ -5,28 +5,46 @@ const modalNav = document.querySelector('.modal-nav');
 const cardsList = document.querySelector('.cards__list');
 const captionTotalCount = document.querySelector('.total-count');
 const captionShowCount = document.querySelector('.show-count');
+const filterList = document.querySelector('.filter__list');
 
-console.log(captionShowCount);
+console.log(filterList);
 renderCardsList(products);
 
 function renderCardsList(prop) {
+    console.log(prop);
     prop.forEach(el => {
         let card = document.createElement('li');
         card.classList.add('cards__item', 'swiper-slide');
-        card.innerHTML = /*html*/`
-        <div class="cards__item-img">
-            <img src=${el.urlImg} alt="${el.name}">
-        </div>
-    
-        <div class="cards__item-title">${el.name}</div>
-    
-        <div class="cards__item-price">$${el.price.priceNow}</div>
-        `;
-        cardsList.append(card);
+        card.dataset.category = el.category;
 
+        card.innerHTML = /*html*/`
+            <div class="cards__item-img">
+            <img src=${el.urlImg} alt=${el.name}>
+            </div>
+            
+            <div class="cards__item-title">
+            ${el.name}
+            </div>
+            
+            <div class="cards__item-price">
+            $${el.price.priceNow}
+            </div>
+            `;
+        cardsList.append(card);
+        // console.log(card.getAttribute('category'));
     });
-    captionTotalCount.innerText = prop.length;
 }
+captionTotalCount.innerText = document.querySelectorAll('.cards__item').length;
+
+// document.querySelector('.filter__list').addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if (e.target.closest('.filter__item').id === null) {
+//         return false;
+//     } else {
+//         console.log(e.target.closest('li').id);
+//         renderCardsList(products);
+//     }
+// });
 
 
 burger.addEventListener("click", function () {
