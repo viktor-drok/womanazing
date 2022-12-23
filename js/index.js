cebe512e62f866bfbebe79e0bca6c6558e85b63d
viktor-drok <vityadrok@gmail.com>
2022-12-23 02:03:27 +0200

    add color option

--- js/index.js
+++ js/index.js
@@ -163,9 +163,10 @@ function renderSize(selectedCard) {
 };
 
 function renderCard(selectedCard) {
-    const { urlImg, price, name, size } = selectedCard;
+    const { urlImg, price, name, size, color } = selectedCard;
 
-    const sizeOption = size.map(el => (` <li>${el}</li> `));
+    const sizeOption = size.map(el => (` <li class="card-size-item">${el}</li> `));
+    const colorOption = color.map(el => (` <li class="card-color-item" style='background: ${el}'></li> `));
 
     console.log(sizeOption.join(''));
     return (/*html*/`
@@ -186,13 +187,13 @@ function renderCard(selectedCard) {
             <div class="card-color">
                 <h3 class="card-color-title">Выберите цвет</h3>
 
-                <div class="card-color-option">dfhfdhdfh</div>
+                <ul class="card-color-option">${colorOption.join('')}</ul>
             </div>
 
             <div class="picked-goods">${name}</div>
             <button type="button" class="add-to-cart"></button>
         </div>
-    </div>;
+    </div>
     `
     );
 };
